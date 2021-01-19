a4932f9b779091597097f077c04b3bf01a201b01
Akshay Patil <akshaypatil240988@gmail.com>
2021-01-19 13:03:56 +0530

    First commit

---
+++ src/DataTypes.java
@@ -0,0 +1,32 @@
+public class DataTypes {
+
+   // Primitive datatypes
+
+    int i=5;
+    boolean user = true;
+    char c = 'A';
+    float f = 0.5f;
+    long l = 20;
+    short s = 6;
+    byte x= 8;
+
+    // Non Primitive datatypes
+
+    Integer o = 9;
+    String z = "Akshay";
+    Character y = 'B';
+    Float v = 0.1f;
+    Double d = 24.09;
+
+    public void getInfo(){
+            System.out.println("hii");
+    }
+
+    public static void main(String[] args) {
+              DataTypes d = new DataTypes();
+              d.getInfo();
+    }
+
+
+}
+
