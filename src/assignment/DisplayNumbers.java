d8056b1793037cd9ababc6ede1f9949249894210
Akshay Patil <akshaypatil240988@gmail.com>
2021-02-04 19:20:18 +0530

    assignment3

--- src/assignment/DisplayNumbers.java
+++ src/assignment/DisplayNumbers.java
@@ -7,11 +7,6 @@ public class DisplayNumbers {
         int num, fact = 1;
 
         Scanner scanner = new Scanner(System.in);
-       /* System.out.println("Enter a number:");
-        num=scanner.nextInt();
-        for(int i=1;i<=10;i++){
-            System.out.println(num*i);
-        }*/
 
         for (int j = 1; j <= 100; j++) {
             System.out.println(j);
@@ -24,38 +19,6 @@ public class DisplayNumbers {
         }
 
         System.out.println("Factorial of " + num + " is " + fact);
-
-
-       /* for (int i = 1; i <= 4; i++) {
-            for (int space = 1; space <= 4 - i;space++) {
-                System.out.print(" ");
-            }
-            while (k != 2 * i - 1) {
-                System.out.print("* ");
-                k++;
-            }
-            System.out.println();
-        }*/
-        for(int i=0;i<4;i++){
-            for(int j=4-i;j>1;j--){
-                System.out.print(" ");
-            }
-            for(int j=0;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-        for(int i=1;i<=4;i++){
-            for(int j=4;j>=i;j--){
-                System.out.print(" ");
-            }
-            for(int k=1;k<=i;k++){
-                System.out.print(" *");
-            }
-            System.out.println();
-        }
-
 
     }
 
---
+++ src/assignment3/NumberTriangle1.java
@@ -0,0 +1,15 @@
+package assignment3;
+
+public class NumberTriangle1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j =4-i;j>=1;j--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
