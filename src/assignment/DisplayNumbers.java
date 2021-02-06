4c7c0c7786ec14bba9d794339344de488fb6a5b8
Akshay Patil <akshaypatil240988@gmail.com>
2021-02-06 20:54:05 +0530

    assignment updated

--- src/assignment/DisplayNumbers.java
+++ src/assignment/DisplayNumbers.java
@@ -4,21 +4,11 @@ import java.util.Scanner;
 
 public class DisplayNumbers {
     public static void main(String[] args) {
-        int num, fact = 1;
-
-        Scanner scanner = new Scanner(System.in);
 
         for (int j = 1; j <= 100; j++) {
             System.out.println(j);
         }
 
-        System.out.println("Enter a number:");
-        num = scanner.nextInt();
-        for (int i = 1; i <= num; i++) {
-            fact = fact * i;
-        }
-
-        System.out.println("Factorial of " + num + " is " + fact);
 
     }
 
