1f9986524523b6f33954d86584655fd072216308
Akshay Patil <akshaypatil240988@gmail.com>
2021-02-09 19:12:10 +0530

    Multithreading lambda expression updated

--- src/multithreading/ThreadDemoLambda.java
+++ src/multithreading/ThreadDemoLambda.java
@@ -1,33 +1,23 @@
 package multithreading;
 
-
-class Hiii implements Runnable {
-
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Hii");
-            try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
-        }
-    }
-}
-
-class Helllo implements Runnable{
-    public void run(){
-        for (int i=1;i<=5;i++){
-            System.out.println("Hello");
-            try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
-        }
-    }
-}
 public class ThreadDemoLambda {
     public static void main(String[] args) {
 
 
-    Runnable hiii = new Hiii();
-    Runnable helllo = new Helllo();
 
-    Thread t1 = new Thread(hiii);
-    Thread t2 = new Thread(helllo);
+    Thread t1 = new Thread(()-> {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hii");
+            try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
+        }
+
+    });
+    Thread t2 = new Thread(()-> {
+        for (int i=1;i<=5;i++){
+            System.out.println("Hello");
+            try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
+        }
+    });
 
         t1.start();
         t2.start();
