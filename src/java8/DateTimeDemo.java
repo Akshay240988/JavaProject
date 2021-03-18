72047a422a4f4edfc84abd75b437b8b2c1e2a12a
Akshay Patil <akshaypatil240988@gmail.com>
2021-03-18 13:06:43 +0530

    Date and time

---
+++ src/java8/DateTimeDemo.java
@@ -0,0 +1,35 @@
+package java8;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class DateTimeDemo {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+
+        LocalDate localDate = LocalDate.now().minusDays(1);
+        LocalDate localDate1 = LocalDate.now();
+        System.out.println(localDate);
+        System.out.println(localDate.getMonth());
+        System.out.println(localDate1.isAfter(localDate));
+        System.out.println(localDate1);
+
+        System.out.println();
+        ZoneId zoneId = ZoneId.of("America/New_York");//or("Asia/Kolkata")
+        System.out.println(zoneId);
+
+        LocalDate localDate2 = LocalDate.now(zoneId);
+        LocalTime localTime = LocalTime.now(zoneId);
+        System.out.println(localDate2);
+        System.out.println(localTime);
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+
+    }
+
+}
